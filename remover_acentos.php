6d44271cab918843aae84465aed12db0f0c4917c
Mauricio <mauricio@casadaweb.net>
2023-01-03 09:13:23 -0300

    Remover Acentos

---
+++ remover_acentos.php
@@ -0,0 +1,39 @@
+<?php
+//REMOVER ACENTOS
+//AUTOR: MAURÍCIO PACHECO
+
+$remover_ecentos = array(
+    'á' => 'a',
+    'à' => 'a',
+    'ã' => 'a',
+    'â' => 'a',
+    'é' => 'e',
+    'ê' => 'e',
+    'í' => 'i',
+    'ó' => 'o',
+    'ô' => 'o',
+    'õ' => 'o',
+    'ú' => 'u',
+    'ü' => 'u',
+    'ç' => 'c',
+    'Á' => 'A',
+    'À' => 'A',
+    'Ã' => 'A',
+    'Â' => 'A',
+    'É' => 'E',
+    'Ê' => 'E',
+    'Í' => 'I',
+    'Ó' => 'O',
+    'Ô' => 'O',
+    'Õ' => 'O',
+    'Ú' => 'U',
+    'Ü' => 'U',
+    'Ç' => 'C'
+);
+
+$nome = "Maurício";
+$nome = strtr($nome, $remover_ecentos);
+
+echo $nome;
+
+?>
